65a4cd87c1d4eaa4611ab040231f10295ed0ffa9
F0r4st3r0 <pjmssb@hotmail.com>
2023-07-17 18:11:33 +0100

    Unidad de tipos de memoria completado

--- Langchain_LLMApplications/2_memory.py
+++ Langchain_LLMApplications/2_memory.py
@@ -1,4 +1,9 @@
 import os
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferWindowMemory
+
 
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv()) # read local .env file
@@ -6,28 +11,27 @@ _ = load_dotenv(find_dotenv()) # read local .env file
 import warnings
 warnings.filterwarnings('ignore')
 
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import ConversationChain
-from langchain.memory import ConversationBufferMemory
+
+
 
 llm = ChatOpenAI(temperature=0.0)
+
 memory = ConversationBufferMemory()
 conversation = ConversationChain(
     llm=llm, 
     memory = memory,
     verbose=False
 )
-
+'''
 print(conversation.predict(input="Hi, my name is Andrew"))
-
 print(conversation.predict(input="What is 1+1?"))
-
 print(conversation.predict(input="What is my name?"))
 
 print("Memory buffer >>>>>>>>>>>>>>>>>>>>>>>>>>")
 print(memory.buffer)
 print("Memory variables >>>>>>>>>>>>>>>>>>>>>>>")
 print(memory.load_memory_variables({}))
+'''
 
 memory = ConversationBufferMemory()
 memory.save_context({"input": "Hi"}, 
@@ -35,8 +39,70 @@ memory.save_context({"input": "Hi"},
 print(memory.buffer)
 print(memory.load_memory_variables({}))
 
+# ConversationBufferWindowMemory
+memory = ConversationBufferWindowMemory(k=1)    
+memory.save_context({"input": "Hi"},
+                    {"output": "What's up"})
+memory.save_context({"input": "Not much, just hanging"},
+                    {"output": "Cool"})
+print(memory.load_memory_variables({}))
 
-memory = ConversationBufferWindowMemory(k=1)               
+llm = ChatOpenAI(temperature=0.0)
+memory = ConversationBufferWindowMemory(k=1)
+conversation = ConversationChain(
+    llm=llm, 
+    memory = memory,
+    verbose=False
+)
+
 print(conversation.predict(input="Hi, my name is Andrew"))
 print(conversation.predict(input="What is 1+1?"))
 print(conversation.predict(input="What is my name?"))
+
+
+
+
+from langchain.memory import ConversationTokenBufferMemory
+from langchain.llms import OpenAI
+llm = ChatOpenAI(temperature=0.0)
+
+memory = ConversationTokenBufferMemory(llm=llm, max_token_limit=30)
+memory.save_context({"input": "AI is what?!"},
+                    {"output": "Amazing!"})
+memory.save_context({"input": "Backpropagation is what?"},
+                    {"output": "Beautiful!"})
+memory.save_context({"input": "Chatbots are what?"}, 
+                    {"output": "Charming!"})
+
+print(memory.load_memory_variables({}))
+
+#ConversationSummaryMemory
+from langchain.memory import ConversationSummaryBufferMemory
+
+# create a long string
+schedule = "There is a meeting at 8am with your product team. \
+You will need your powerpoint presentation prepared. \
+9am-12pm have time to work on your LangChain \
+project which will go quickly because Langchain is such a powerful tool. \
+At Noon, lunch at the italian resturant with a customer who is driving \
+from over an hour away to meet you to understand the latest in AI. \
+Be sure to bring your laptop to show the latest LLM demo."
+
+memory = ConversationSummaryBufferMemory(llm=llm, max_token_limit=100)
+memory.save_context({"input": "Hello"}, {"output": "What's up"})
+memory.save_context({"input": "Not much, just hanging"},
+                    {"output": "Cool"})
+memory.save_context({"input": "What is on the schedule today?"}, 
+                    {"output": f"{schedule}"})
+
+print(memory.load_memory_variables({}))
+
+conversation = ConversationChain(
+    llm=llm, 
+    memory = memory,
+    verbose=True
+)
+
+conversation.predict(input="What would be a good demo to show?")
+
+print(memory.load_memory_variables({}))
