6affdbc6fd80d97102c3d83d7a288098b19d0b5d
F0r4st3r0 <pjmssb@hotmail.com>
2023-07-25 22:13:50 +0100

    Fin de unidad 6

--- Langchain_LLMApplications/6_Agents.py
+++ Langchain_LLMApplications/6_Agents.py
@@ -15,3 +15,71 @@ from langchain.chat_models import ChatOpenAI
 
 llm = ChatOpenAI(temperature=0)
 
+tools = load_tools(["llm-math","wikipedia"], llm=llm)
+
+agent= initialize_agent(
+    tools, 
+    llm, 
+    agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+    handle_parsing_errors=True,
+    verbose = True)
+
+#agent("What is the 25% of 300?")
+#agent("What is the square root of 27?")
+
+question = "Tom M. Mitchell is an American computer scientist \
+and the Founders University Professor at Carnegie Mellon University (CMU)\
+what book did he write?"
+#result = agent(question) 
+
+
+agent = create_python_agent(
+    llm,
+    tool=PythonREPLTool(),
+    verbose=True
+)   
+
+customer_list = [["Harrison", "Chase"], 
+                 ["Lang", "Chain"],
+                 ["Dolly", "Too"],
+                 ["Elle", "Elem"], 
+                 ["Geoff","Fusion"], 
+                 ["Trance","Former"],
+                 ["Jen","Ayai"]
+                ]
+
+import langchain
+langchain.debug=True
+#agent.run(f"""Sort these customers by \
+#last name and then first name \
+#and print the output: {customer_list}""") 
+langchain.debug=False   
+
+
+#Generar una herramienta ad-hoc
+
+from langchain.agents import tool
+from datetime import date
+
+@tool
+def time(text: str) -> str:
+    """Returns todays date, use this for any \
+    questions related to knowing todays date. \
+    The input should always be an empty string, \
+    and this function will always return todays \
+    date - any date mathmatics should occur \
+    outside this function."""
+    return str(date.today())
+
+agent= initialize_agent(
+    tools + [time], 
+    llm, 
+    agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+    handle_parsing_errors=True,
+    verbose = True)
+
+try:
+    result = agent("whats the date today?") 
+except: 
+    print("exception on external access")
+
