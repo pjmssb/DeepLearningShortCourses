870e45dc28fa3b9e94c0a82999286e50f5282f72
F0r4st3r0 <pjmssb@hotmail.com>
2023-07-26 11:15:19 +0100

    Parcial - Ojo con ffprobe y ffmepg

--- Langchain_chatWithYourData/1_document_loading.py
+++ Langchain_chatWithYourData/1_document_loading.py
@@ -7,6 +7,8 @@ This is useful if we want to ask question about specific
 documents (e.g., our PDFs, a set of videos, etc).
 '''
 
+
+#PDF
 import os
 import openai
 import sys
@@ -20,3 +22,23 @@ openai.api_key  = os.environ['OPENAI_API_KEY']
 from langchain.document_loaders import PyPDFLoader
 loader = PyPDFLoader("./Langchain_chatWithYourData/MachineLearning-Lecture01.pdf")
 pages = loader.load()
+
+print(len(pages))
+page = pages[0]
+print(page.page_content[0:500])
+print(page.metadata)
+
+#Youtube
+# ! pip install yt_dlp
+# ! pip install pydub
+from langchain.document_loaders.generic import GenericLoader
+from langchain.document_loaders.parsers import OpenAIWhisperParser
+from langchain.document_loaders.blob_loaders.youtube_audio import YoutubeAudioLoader
+
+url="https://www.youtube.com/watch?v=jGwO_UgTS7I"
+save_dir="./DeepLearningShortCourses/"
+loader = GenericLoader(
+    YoutubeAudioLoader([url],save_dir),
+    OpenAIWhisperParser()
+)
+docs = loader.load()
