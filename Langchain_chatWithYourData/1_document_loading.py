62ba13bfd6256c544af396346800ae4f6b70ac13
F0r4st3r0 <pjmssb@hotmail.com>
2023-07-27 10:48:29 +0100

    Comnpleting the unit 2

--- Langchain_chatWithYourData/1_document_loading.py
+++ Langchain_chatWithYourData/1_document_loading.py
@@ -8,7 +8,6 @@ documents (e.g., our PDFs, a set of videos, etc).
 '''
 
 
-#PDF
 import os
 import openai
 import sys
@@ -19,6 +18,10 @@ _ = load_dotenv(find_dotenv()) # read local .env file
 
 openai.api_key  = os.environ['OPENAI_API_KEY']
 
+
+
+#PDF
+'''
 from langchain.document_loaders import PyPDFLoader
 loader = PyPDFLoader("./Langchain_chatWithYourData/MachineLearning-Lecture01.pdf")
 pages = loader.load()
@@ -27,19 +30,22 @@ print(len(pages))
 page = pages[0]
 print(page.page_content[0:500])
 print(page.metadata)
+'''
+
 
 #Youtube
 # ! pip install yt_dlp
 # ! pip install pydub
 # Windows Command  python .\Langchain_chatWithYourData\1_document_loading.py --ffmpeg-location .\Langchain_chatWithYourData\ff-essentials-6.0\bin\
-# ffmpeg instalado especialmente. No lo incluyo en 
-
+# ffmpeg instalado especialmente, se ignora en github y NO LOGRÉ HACERLO FUNCIONAR EN WINDOWS
+# Código del notebook del curso también falla
+'''
 from langchain.document_loaders.generic import GenericLoader
 from langchain.document_loaders.parsers import OpenAIWhisperParser
-from langchain.document_loaders.blob_loaders.youtube_audio import YoutubeAudioLoaderff
+from langchain.document_loaders.blob_loaders.youtube_audio import YoutubeAudioLoader
 
 url="https://www.youtube.com/watch?v=jGwO_UgTS7I"
-save_dir="./DeepLearningShortCourses/"
+save_dir="./Langchain_chatWithYourData"
 loader = GenericLoader(
     YoutubeAudioLoader([url],save_dir),
     OpenAIWhisperParser()
@@ -47,3 +53,50 @@ loader = GenericLoader(
 docs = loader.load()
 
 docs[0].page_content[0:500]
+'''
+
+
+#URLs
+'''
+print(f"Este es el inicio de carga de URLs")
+from langchain.document_loaders import WebBaseLoader
+
+loader = WebBaseLoader("https://github.com/basecamp/handbook/blob/master/37signals-is-you.md")
+#loader = WebBaseLoader("https://miasmoda.cl/") #--> Acceso no autorizado ????!!!!!!!!!
+#loader = WebBaseLoader("https://miasmoda.cl/products/body-sabina") #--> Acceso no autorizado ????!!!!!!!!!
+docs = loader.load()
+print(docs[0].page_content[:500])
+'''
+
+
+#Notion
+# Hay que construir un sitio así que se probó el código en el 
+# notebook del curso
+# Se deja aquí como referencia
+'''
+from langchain.document_loaders import NotionDirectoryLoader
+loader = NotionDirectoryLoader("docs/Notion_DB")
+docs = loader.load()
+print(docs[0].page_content[0:200])
+docs[0].metadata
+'''
+
+
+#URL with Beautiful Soup
+# Check this: https://www.geeksforgeeks.org/implementing-web-scraping-python-beautiful-soup/
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://www.geeksforgeeks.org/data-structures/"
+headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"}
+# Here the user agent is for Edge browser on windows 10. You can find your browser user agent from the above given link.
+r = requests.get(url=URL, headers=headers)
+print(r.content)
+soup = BeautifulSoup(r.content, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib
+print(soup.prettify())
+'''
+from langchain.document_loaders import BSHTMLLoader
+loader = BSHTMLLoader("https://miasmoda.cl/products/body-sabina")
+data = loader.load()
+print(data)
+'''
---
+++ Langchain_chatWithYourData/2_document_splitting.py
@@ -0,0 +1,88 @@
+import os
+import openai
+import sys
+sys.path.append('../..')
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+openai.api_key  = os.environ['OPENAI_API_KEY']
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
+chunk_size =26
+chunk_overlap = 4
+
+r_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=chunk_size,
+    chunk_overlap=chunk_overlap
+)
+c_splitter = CharacterTextSplitter(
+    chunk_size=chunk_size,
+    chunk_overlap=chunk_overlap
+)
+
+text1 = 'abcdefghijklmnopqrstuvwxyz'
+print(r_splitter.split_text(text1))
+
+text2 = 'abcdefghijklmnopqrstuvwxyzabcdefg'
+print(r_splitter.split_text(text2))
+
+text3 = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
+print(r_splitter.split_text(text3))
+print(c_splitter.split_text(text3))
+
+
+c_splitter = CharacterTextSplitter(
+    chunk_size=chunk_size,
+    chunk_overlap=chunk_overlap,
+    separator = ' '
+)
+print(c_splitter.split_text(text3))
+
+#Recursive splitting details
+#RecursiveCharacterTextSplitter is recommended for generic text.
+
+some_text = """When writing documents, writers will use document structure to group content. \
+This can convey to the reader, which idea's are related. For example, closely related ideas \
+are in sentances. Similar ideas are in paragraphs. Paragraphs form a document. \n\n  \
+Paragraphs are often delimited with a carriage return or two carriage returns. \
+Carriage returns are the "backslash n" you see embedded in this string. \
+Sentences have a period at the end, but also, have a space.\
+and words are separated by space."""
+
+print(len(some_text))
+
+c_splitter = CharacterTextSplitter(
+    chunk_size=450,
+    chunk_overlap=0,
+    separator = ' '
+)
+r_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=450,
+    chunk_overlap=0, 
+    separators=["\n\n", "\n", " ", ""]
+)
+
+print(c_splitter.split_text(some_text))
+
+print(r_splitter.split_text(some_text))
+
+print("\n\n\nLet's reduce the chunk size a bit and add a period to our separators:")
+r_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=150,
+    chunk_overlap=0,
+    separators=["\n\n", "\n", "\. ", " ", ""]
+)
+print(r_splitter.split_text(some_text))
+
+
+r_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=150,
+    chunk_overlap=0,
+    separators=["\n\n", "\n", "(?<=\. )", " ", ""]
+)
+print(r_splitter.split_text(some_text))
+
+from langchain.document_loaders import PyPDFLoader
+loader = PyPDFLoader("docs/cs229_lectures/MachineLearning-Lecture01.pdf")
+pages = loader.load()
