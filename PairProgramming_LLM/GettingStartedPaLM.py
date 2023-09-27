3a61de10b269498ed0cf4f034210f3f8c3f8e29f
F0r4st3r0 <pjmssb@hotmail.com>
2023-09-27 22:21:56 +0100

    iniciando nuevo curso

--- PairProgramming_LLM/GettingStartedPaLM.py
+++ PairProgramming_LLM/GettingStartedPaLM.py
@@ -1,1 +1,26 @@
-from utils import get_api_key
+import os
+import sys
+sys.path.append('../..')
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+api_key  = os.environ['PALM_API_KEY']
+
+
+import os
+import google.generativeai as palm
+from google.api_core import client_options as client_options_lib
+
+palm.configure(
+    api_key,
+    transport="rest",
+    client_options=client_options_lib.ClientOptions(
+        api_endpoint=os.getenv("GOOGLE_API_BASE"),
+    )
+)
+
+for m in palm.list_models():
+    print(f"name: {m.name}")
+    print(f"description: {m.description}")
+    print(f"generation methods:{m.supported_generation_methods}\n")
